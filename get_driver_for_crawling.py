0abb171ccccd85aae0fbd4aa56be66710cdf0157
ioqoo <59276856+ioqoo@users.noreply.github.com>
2020-11-07 21:14:37 +0900

    data_syllabus_1
    망함

--- get_driver_for_crawling.py
+++ get_driver_for_crawling.py
@@ -12,7 +12,7 @@ def get_syllabus_popup_selector(n):
 CURRI_LINK = 'http://ysweb.yonsei.ac.kr:8888/curri120601/curri_new.jsp#top'
 class_info_title = ['학정번호', '과목명', '학점', '담당교수', '강의시간', '강의실', '정원', '참여인원', '전공자정원', '1학년정원', '2학년정원', '3학년정원', '4학년정원', '교환가능여부', '최대마일리지', '최소값', '최대값', '평균값']
 mileage_rank_title = ['순위', '마일리지', '전공자여부', '신청과목수', '졸업신청', '초수강여부', '총이수/졸업이수', '직전학기이수/학기당수강', '학년', '수강여부', '비고']
-year_list = ['2017', '2018', '2019', '2020']
+year_list = ['2019', '2020']
 semester_list = ['1학기', '2학기']
 
 search_selector = '#myForm > table > tbody > tr:nth-child(1) > td:nth-child(1) > a:nth-child(6) > img'
@@ -23,7 +23,13 @@ semester_selector = '#HG'
 pageinfo_selector = '#pager > div > div > div:nth-child(3)'
 nextpage_selector = '/html/body/div[5]/form[2]/table/tbody/tr[3]/td/div/div[2]/div/div[8]/div/div/div[2]/div'
 
+# options = webdriver.ChromeOptions()
+# options.add_argument('headless')
+# options.add_argument('window-size=1920x1080')
+# options.add_argument("disable-gpu")
+
 driver = webdriver.Chrome('C:\chromedriver')
+# driver = webdriver.Chrome('C:\chromedriver', chrome_options=options)
 driver.get(CURRI_LINK)
 SELECTOR = driver.find_element_by_css_selector
 SELECTOR_XPATH = driver.find_element_by_xpath
